43527e1666c1dac313f58443a0cc4e2303aac818
john gallagher <127highstreet@gmail.com>
2021-10-19 12:03:10 -0400

    bhutan update before lunch

---
+++ scripts/Bhutan/celebrities/CelebritiesDateProvider.js
@@ -0,0 +1,7 @@
+celebritiesCollection =  [
+    {  id: 1,
+       name: "Jigme Dorji WangchuckJigme Singye Wangchuck",
+       image: "https://en.wikipedia.org/wiki/File:Jigme_Singye_Wangchuck_2008.jpg",
+       description: "Jigme Singye Wangchuck is the former King of Bhutan. He was the fourth Dragon King of Bhutan from 1972 until his abdication in favour of his eldest son, Jigme Khesar Namgyel Wangchuck. During his reign, he advocated the use of a Gross National Happiness index to measure the well-being of citizens rather than Gross domestic product."
+   },
+]
